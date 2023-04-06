e308d9069343f15bd0b5d984073e573993948308
unknown <akimtemun9@gmail.com>
2023-04-06 20:58:27 +0800

    tugas phiton1

--- tugas if.py
+++ tugas if.py
@@ -1,4 +1,4 @@
-nilai_ujian = [25, 90, 40, 95, 35, 70, 50, 20, 80, 65]
+nilai_ujian = [25, 90, 40, 100, 35, 70, 50, 20, 80, 65]
 nilai_terbesar = 0
 for nilai in nilai_ujian:
     if nilai > nilai_terbesar:
